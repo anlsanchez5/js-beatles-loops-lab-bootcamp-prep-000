c1ebb0fcf876e50543b3b51ebde7bfc015b7437f
anlsanchez@ucdavis.edu <anlsanchez@ucdavis.edu>
2018-04-11 06:26:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,10 +18,9 @@ function johnLennonFacts(facts) {
 
 function iLoveTheBeatles(number) {
   var nothing = [];
-  var i = number
   do {
     nothing.push("I love the Beatles!");
-    i++
+    number++
   }
   while (number<15)
   }
