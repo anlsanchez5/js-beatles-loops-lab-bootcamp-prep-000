f2e4f13ef32e8124eae92da950e275a240e15b38
anlsanchez@ucdavis.edu <anlsanchez@ucdavis.edu>
2018-04-11 05:40:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function theBeatlesPlay(musc, inst) {
+  var empty = []
+  for (var i = 0; i < musc.length; i++) {
+    empty.push(musc[i] + " plays " + inst[i])
+  } 
+  return empty
+}
+
+function johnLennonFacts(facts) {
+  var x = 0;
+  while (x<facts.length) {
+    facts[i] = facts[i] + "!!!"; y++
+  }
+  return facts
+}
+  
